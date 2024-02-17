1619a1ceafc7d672bb293f938b347e6e9db25fd2
Juan J. Rojas <48027894+juanjorojash@users.noreply.github.com>
2024-02-17 16:32:24 -0600

    datos

---
+++ generar_malla.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from pylatex import Document, Package, Command, PageStyle, Head, Foot, NewPage,\
+    TextColor, MiniPage, StandAloneGraphic, simple_page_number,\
+    TikZ, TikZNode, TikZOptions, TikZCoordinate, TikZNodeAnchor, TikZPath,\
+    UnsafeCommand,\
+    VerticalSpace, HorizontalSpace, NewLine,\
+    LongTable
+from pylatex.base_classes import Environment, Arguments
+from pylatex.utils import NoEscape, bold, italic
+
