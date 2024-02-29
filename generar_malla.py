e8ba902fa7a57779587fbe650318519889673954
Juan J. Rojas <48027894+juanjorojash@users.noreply.github.com>
2024-02-28 21:19:06 -0600

    masomenos

--- generar_malla.py
+++ generar_malla.py
@@ -1,4 +1,5 @@
 import os
+import roman
 import pandas as pd
 from pylatex import Document, Package, Command, PageStyle, Head, Foot, NewPage,\
     TextColor, MiniPage, StandAloneGraphic, simple_page_number,\
@@ -9,7 +10,13 @@ from pylatex import Document, Package, Command, PageStyle, Head, Foot, NewPage,\
 from pylatex.base_classes import Environment, Arguments
 from pylatex.utils import NoEscape, bold, italic
 
-datos = pd.read_csv("malla_EM.csv")
+datos = pd.read_csv("aeronautica.csv")
+
+datos.Semestre = datos.Semestre.astype(int)
+datos.Columna = datos.Semestre.astype(int)
+datos.HorasTeoria = datos.HorasTeoria.astype(int)
+datos.HorasPractica = datos.HorasPractica.astype(int)
+datos.Creditos = datos.Creditos.astype(int)
 
 def textcolor(size,vspace,color,bold,text,hspace="0"):
     dump = NoEscape(r"\par")
@@ -23,9 +30,19 @@ def textcolor(size,vspace,color,bold,text,hspace="0"):
         dump += NoEscape(Command("textcolor",arguments=Arguments(color,text)).dumps())
     return dump
 
+def colocar_titulo(titulo,color):
+    dump = NoEscape(f"\draw ({round((5*9)/2,1)},{round(0)})")
+    dump += NoEscape(f"pic{{titulo={{{titulo},{color}}}}};")
+    return dump
+
 def colocar_curso(codigo,nombre,columna,semestre,horasteoria,horaspractica,creditos,color):
     dump = NoEscape(f"\draw ({round(5*columna)},{round(-4*semestre)})")
     dump += NoEscape(f"pic{{curso={{{codigo},{nombre},{round(horasteoria)},{round(horaspractica)},{round(creditos)},{color}}}}};")
+    return dump
+
+def colocar_semestre(semestre,color):
+    dump = NoEscape(f"\draw ({round(0)},{round(-4*semestre)})")
+    dump += NoEscape(f"pic{{semestre={{{roman.toRoman(semestre)},{color}}}}};")
     return dump
 
 def generar_malla(programa):
@@ -47,7 +64,7 @@ def generar_malla(programa):
                    textcomp=False, \
                    page_numbers=True, \
                    indent=False, \
-                   document_options=["letterpaper","lanscape"],
+                   document_options=["letterpaper","landscape"],
                    geometry_options=geometry_options)
     #Packages
     doc.packages.append(Package(name="fontspec", options=None))
@@ -63,14 +80,28 @@ def generar_malla(programa):
     doc.packages.append(Package(name="longtable"))
     doc.packages.append(Package(name="multirow"))
     doc.packages.append(Package(name="fancyhdr"))
-    #Prueba
+    
+    #bloques
+
+    bloqueTitulo = NoEscape(
+    r'''\tikzset{
+            pics/titulo/.style args={#1,#2}{
+            code={
+                \def\ancho{45}
+                \def\alto{0.7}
+                \draw[fill=#2] (-\ancho/2-2,2*\alto) rectangle (\ancho/2+2,-2*\alto) node[midway,align=center,text width=45cm]{\fontsize{40pt}{0pt}\selectfont \textbf{#1}};
+            }
+        }
+    }'''
+    )
+
     bloqueCurso = NoEscape(
     r'''\tikzset{
             pics/curso/.style args={#1,#2,#3,#4,#5,#6}{
             code={
                 \def\ancho{4}
                 \def\alto{0.7}
-                \draw[fill=#6] (-\ancho/2,\alto) rectangle (\ancho/2,-\alto) node[midway,align=center,text width=4cm]{\fontsize{10pt}{12pt}\selectfont \textbf{#2}};
+                \draw[fill=#6] (-\ancho/2,\alto) rectangle (\ancho/2,-\alto) node[midway,align=center,text width=4cm]{\fontsize{10pt}{12pt}\selectfont {#2}};
                 \draw[fill=#6] (-\ancho/2,\alto) rectangle (\ancho/2,\alto + \alto) node[midway]{\fontsize{12pt}{14pt}\selectfont #1};
                 \draw[fill=#6] (-\ancho/2,-\alto) rectangle (-\ancho/2 + \ancho/3, -\alto - \alto) node[midway]{\fontsize{12pt}{14pt}\selectfont #3};
                 \draw[fill=#6] (-\ancho/2 + \ancho/3,-\alto) rectangle (-\ancho/2 + 2*\ancho/3, -\alto - \alto) node[midway]{\fontsize{12pt}{14pt}\selectfont #4};
@@ -79,19 +110,39 @@ def generar_malla(programa):
         }
     }'''
     )
-        
+
+    bloqueSemestre = NoEscape(
+    r'''\tikzset{
+            pics/semestre/.style args={#1,#2}{
+            code={
+                \def\ancho{4}
+                \def\alto{0.7}
+                \draw[fill=#2] (-\ancho/2,2*\alto) rectangle (\ancho/2,-2*\alto) node[midway,align=center,text width=4cm]{\fontsize{10pt}{12pt}\selectfont \textbf{#1}};
+            }
+        }
+    }'''
+    )
+
+    doc.preamble.append(bloqueTitulo)        
     doc.preamble.append(bloqueCurso)
-    
+    doc.preamble.append(bloqueSemestre)
+    doc.append(Command('centering'))
     with doc.create(TikZ(
             options=TikZOptions
                 (    
-                "scale = 0.4",
+                "scale = 0.5",
                 "transform shape"
                 )
         )) as malla:
+        # malla.append(NoEscape(r"\draw (,0)--(45,-2);"))
+        malla.append(colocar_titulo(f"Licenciatura en Ingeniería {programa}","lightgray"))
+        for semestre in range(1,11):
+            malla.append(colocar_semestre(semestre,"lightgray"))
         for codigo in cursos.Codigo:
             nombre = cursos[cursos.Codigo == codigo].Nombre.item()
             columna = cursos[cursos.Codigo == codigo].Columna.item()
+            print(codigo)
+            print(columna)
             semestre = cursos[cursos.Codigo == codigo].Semestre.item()
             horasteoria = cursos[cursos.Codigo == codigo].HorasTeoria.item()
             horaspractica = cursos[cursos.Codigo == codigo].HorasPractica.item()
@@ -106,15 +157,13 @@ def generar_malla(programa):
                     color = "purple"
                 case "Básicas":
                     color = "lime"
-
+                case "-":
+                    color = "white"
 
             malla.append(colocar_curso(codigo,nombre,columna,semestre,horasteoria,horaspractica,creditos,color))
          
     doc.generate_pdf(f"./mallas/{programa}", clean=True, clean_tex=False, compiler='lualatex')
 
 
-# programa = "Electromecánica"
-# cursos = datos[datos.Programa == programa]
-# codigo = "FI2103"
-# print(cursos[cursos.Codigo == codigo].Columna.item())
-generar_malla("Electromecánica")
+#generar_malla("Electromecánica")
+generar_malla("Aeronáutica")
