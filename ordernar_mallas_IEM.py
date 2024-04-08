d06da433b5c3cd5f4e48c66f9628c9e96a22ed4e
Juan J. Rojas <48027894+juanjorojash@users.noreply.github.com>
2024-04-07 21:15:03 -0600

    Mante con nueve columnas

--- ordernar_mallas_IEM.py
+++ ordernar_mallas_IEM.py
@@ -3,7 +3,7 @@ import pandas as pd
 mallas = pd.read_csv("mallas_IEM.csv",
     dtype = {'Codigo':str,'Programa':str,'Plan':str,'Semestre':int,'Columna':int,'HorasTeoria':int,'HorasPractica':int,'Creditos':int})
 
-mallas.sort_values(by=['Codigo'], ascending=True, inplace=True)
+mallas.sort_values(by=['Programa','Semestre','Columna'], ascending=True, inplace=True)
 
 
 mallas.to_csv('mallas_IEM.csv', index=False, encoding='utf-8-sig')
